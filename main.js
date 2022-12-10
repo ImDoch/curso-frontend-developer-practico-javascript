3f2a13cc3972ca3450f826f145b0b8f72db03e90
Jeison Gomez <gomezres011@gmail.com>
2022-12-09 21:41:46 -0500

    integrando mobile menu funcional

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('innactive') //El metodo toggle alterna el remover y poner la clase segun si el elemento tiene la clase o no la tiene en el momento de hacer click en el elemento
+}
+
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('innactive');
 }
