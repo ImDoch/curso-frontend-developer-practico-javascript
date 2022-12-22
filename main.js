7f41cf3154bd9c4e326e84d4604c683e05b7d934
Jeison Gomez <gomezres011@gmail.com>
2022-12-22 14:38:57 -0500

    product list desde el js

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCartIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail')
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container')
+
+//Mostrando y ocultando partes del html con event listeners
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
@@ -24,3 +27,61 @@ function toggleCartAside () {
     mobileMenu.classList.add('innactive'); //Oculta el mobileMenu cuando se clickea el carrito
     aside.classList.toggle('innactive'); //Alterna entre mostrar y ocultar el aside(My Order) cuando se clickea el carrito
 }
+
+//Creando HTML desde JS
+
+//Aqui estamos creando un arreglo de objetos, estos objetos contienen la informacion de cada producto que se venderá en la pagina
+const productList = [];
+productList.push({
+    name: 'Byke',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Screen',
+    price: 220,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push({
+    name: 'Laptop',
+    price: 500,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+//Aqui estamos recorriendo el arreglo donde por cada elemento, en este caso, producto, se creará un div en el documento HTML.
+function renderProducts(array) {
+    for (product of array) {  //Es una forma abreviada de usar el ciclo for: for ( |cualquier nombre| of |lista que queremos recorrer| ), donde 'cualquier nombre' es cada uno de los elementos del array.
+
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfoDiv.append(productPrice, productName);
+
+        productInfo.append(productInfoDiv, productInfoFigure);
+
+        productCard.append(productImg, productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+
+}
+renderProducts(productList);
